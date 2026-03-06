309a7ca97b99b4920be83b10eece64f2ad882260
Hemamalini-L <hemamalini291204@gmail.com>
2026-03-06 18:07:47 +0530

    Update alert_system.py

--- backend/services/alert_system.py
+++ backend/services/alert_system.py
@@ -1,1 +1,13 @@
+def generate_alert(threat):
 
+    if threat == "Critical Intrusion":
+        return "🚨 DEFENCE ALERT: Possible Cyber Attack Detected"
+
+    elif threat == "Medium Risk Attack":
+        return "⚠️ Suspicious Activity Detected"
+
+    elif threat == "Low Risk Threat":
+        return "⚡ Minor Network Anomaly"
+
+    else:
+        return "✅ Network Safe"
